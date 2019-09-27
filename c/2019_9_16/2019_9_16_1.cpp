7de8e88ea14ec0ae8343d00f992fc55ac445d9cb
HONGYU-LEE <756687451@qq.com>
2019-09-27 21:28:50 +0800

    2019_9_27/test1

--- c/2019_9_16/2019_9_16_1.cpp
+++ c/2019_9_16/2019_9_16_1.cpp
@@ -1,14 +1,23 @@
 #include<stdio.h>
+#include<math.h> 
 int main()
 {
-	int i,j;
-	for(i=100; i<=200; i++){
-		for(j=2; j<=i; j++)
-			if( i%j == 0){
+	int i,j,count=0;
+	for( i=100; i<=200; i++)
+	{
+		for( j=2; j<=sqrt(i); j++)
+		{ 
+			if( 0 == i%j )
+			{
 				break;
 			}
-		if( i == j )
-			printf("%d\t",i);
+		}
+		if( j > sqrt(i) )
+		{
+			printf("%d ",i);
+			count++;
+		}
 	}
+	printf("\n素数的个数为：%d个",count); 
 	return 0;
 }
