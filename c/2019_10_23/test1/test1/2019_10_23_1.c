8d7ae60bbe9847eca2a4803faeceea84d1795eb0
HONGYU-LEE <756687451@qq.com>
2019-10-23 23:11:57 +0800

    2019_10_23

---
+++ c/2019_10_23/test1/test1/2019_10_23_1.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	int i = 1;
+	char ch = (char)i;
+	if (ch == 0)
+		printf("´ó¶Ë´æ´¢\n");
+	else if (ch == 1)
+		printf("Ð¡¶Ë´æ´¢\n");
+	return 0;
+
+}
