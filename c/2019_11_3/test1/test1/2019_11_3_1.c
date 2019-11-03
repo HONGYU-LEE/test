41434876981f595f02bfb1d98b4ceaed05a822f4
HONGYU-LEE <756687451@qq.com>
2019-11-03 20:51:39 +0800

    2019_11_3_1

---
+++ c/2019_11_3/test1/test1/2019_11_3_1.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+unsigned int reverse_bit(unsigned int value)
+{
+	unsigned int ret = 0, i;
+	for (i = 0; i < 32; i++)
+	{
+		ret += ((value >> i) & 1) * pow(2, 31 - i);
+	}
+	return ret;
+}
+
+int main()
+{
+	unsigned int value;
+	printf("请输入要反转二进制位的数字：\n");
+	scanf("%u", &value);
+	printf("反转后:%u\n ", reverse_bit(value));
+	return 0;
+}
