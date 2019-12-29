0e673785b0b9aa46891088cc025e395ce8e0a040
HONGYU-LEE <756687451@qq.com>
2019-12-29 23:21:11 +0800

    给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。

---
+++ leetcode/leetcode-189/test.c
@@ -0,0 +1,18 @@
+void reverse(int *arr, int start, int end)
+{
+    while(start < end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+void rotate(int* nums, int numsSize, int k)
+{
+    k %= numsSize;
+    reverse(nums, 0, numsSize - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, numsSize - 1);
+}
