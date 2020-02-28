a26ffee109474d734e7cd7066c5767ecde7cf2e2
HONGYU-LEE <756687451@qq.com>
2020-02-28 22:48:05 +0800

    堆排序

---
+++ data structure/Sort/HeapSort/HeapSort.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+
+void AdjustDown(int *arr, int size, int root)
+{
+	int parent = root;
+	int child = parent * 2 + 1;
+	
+	while(child < size)
+	{
+		if(child + 1 < size && arr[child] > arr[child + 1])
+		{
+			++child;
+		}
+		
+		if(arr[child] < arr[parent])
+		{
+			int temp = arr[parent];
+			arr[parent] = arr[child];
+			arr[child] = temp;
+		}
+		else
+			break;
+
+		
+		parent = child;		
+		child = parent * 2 + 1;
+	}
+}
+
+void HeapSort(int *arr, int size)
+{
+	int i = 0;
+	for(i = (size - 2) / 2 ; i >= 0; i--)
+	{
+		AdjustDown(arr, size, i);
+	}
+	
+	for(i = size - 1; i > 0; i--)
+	{		
+		int temp = arr[0];
+		arr[0] = arr[i];
+		arr[i] = temp;
+		AdjustDown(arr, i, 0);
+	}
+}
+
+int main()
+{
+	int arr[10] = {46, 74, 53, 14, 26, 36, 86, 65, 27, 34};
+	int length = sizeof(arr) / sizeof(arr[0]);
+	int i = 0;
+	HeapSort(arr, length); 
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+} 
