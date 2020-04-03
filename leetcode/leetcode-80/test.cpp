c1dac9018efa43bc71b6a695a11154954c6331b2
HONGYU-LEE <756687451@qq.com>
2020-04-03 22:26:17 +0800

    leetcode-80. 删除排序数组中的重复项 II 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素最多出现两次，返回移除后数组的新长度。

---
+++ leetcode/leetcode-80/test.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() < 3)
+            return nums.size();
+
+        int k = 1;
+        for(int i = 2; i < nums.size(); i++)
+        {
+            if(nums[i] != nums[k - 1])
+            {
+                nums[++k] = nums[i];
+            }
+        }
+
+        return k + 1;
+    }
+};
