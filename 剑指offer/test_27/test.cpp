aec8dd6390ff27fe34a53273cd9b256d4d9edd97
HONGYU-LEE <756687451@qq.com>
2020-04-25 22:08:40 +0800

    27.二叉树的镜像 请完成一个函数，输入一个二叉树，该函数输出它的镜像。

---
+++ 剑指offer/test_27/test.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mirrorTree(TreeNode* root) {
+        if(!root)
+            return nullptr;
+
+        mirrorTree(root->right);
+        mirrorTree(root->left);
+
+        swap(root->right, root->left);
+
+        return root;
+    }
+};
