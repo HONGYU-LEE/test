02e077a8e92aa39a07fa2087f521ae8597ec1579
HONGYU-LEE <756687451@qq.com>
2020-07-02 23:14:20 +0800

    C++实现红黑树

---
+++ data structure/Tree/RBTree/RBTree/test.cpp
@@ -0,0 +1,50 @@
+#include"RBTree.hpp"
+
+using namespace std;
+template<class K, class V>
+struct KeyOfValue
+{
+	const K& operator()(const std::pair<K, V>& kv)
+	{
+		return kv.first;
+	}
+};
+
+//int main()
+//{
+//	//int a[] = { 16, 3, 7, 11, 9, 26, 18, 14, 15 };
+//	int a[] = { 4, 2, 6, 1, 3, 5, 15, 7, 16, 14 };
+//	RBTree<int, int, KeyOfValue<int, int>> t;
+//	for (auto e : a)
+//	{
+//		t.Insert(make_pair(e, e));
+//	}
+//
+//	t.InOrder();
+//	cout << t.IsValidRBTree() << endl;
+//}
+enum Color
+{
+	BLACK,
+	RED,
+};
+
+int main()
+{
+	lee::RBTree<int, pair<int, int> , KeyOfValue<int, int>> t;
+	int a[] = { 4, 2, 6, 1, 3, 5, 15, 7, 16, 14 };
+	for (auto e : a)
+	{
+		t.Insert(make_pair(e, e));
+	}
+
+
+	for (auto it : t)
+	{
+		cout << it.first << endl;
+	}
+
+
+
+	return 0;
+}
