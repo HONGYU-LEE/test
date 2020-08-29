36080c38b94d4ddcb689bf89d8afb34b2f07b40f
HONGYU-LEE <756687451@qq.com>
2020-08-29 23:42:07 +0800

    剑指 Offer 50. 第一个只出现一次的字符

---
+++ 剑指offer/test_50/test.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    char firstUniqChar(string s) {
+        int hash[26] = {0};
+
+        for(auto ch : s)
+        {
+            hash[ch - 'a']++;
+        }
+
+        for(auto ch : s)
+        {
+            if(hash[ch - 'a'] == 1)
+                return ch;
+        }
+
+        return ' ';
+    }
+};
